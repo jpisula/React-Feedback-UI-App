6c492f8145f4ac53b832d5c473799f7f51a5e3ad
Jakub Pisula <jakubpisula@macbook-pro-jakub-1.home>
2021-12-26 23:09:51 +0100

    Production version of app, without api connected, just structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AboutIconLink from './components/AboutIconLink';
+import FeedbackForm from './components/FeedbackForm';
 import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
-import FeedbackData from './data/FeedbackData';
+import { FeedbackProvider } from './context/FeedbackContext';
+import AboutPage from './pages/AboutPage';
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
   return (
-    <>
-      <Header />
-      <div className='container'>
-        <FeedbackList feedback={feedback} />
-      </div>
-    </>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              exact
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
